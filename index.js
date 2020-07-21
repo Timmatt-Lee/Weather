dbda5195a22706d9d24d3e2f413696b3e454624c
Timmatt <timmatt@ntu.im>
2020-07-21 19:51:16 +0800

    feat: parse weather data

--- index.js
+++ index.js
@@ -9,8 +9,30 @@ app.use(cors());
 
 app.use(async (ctx) => {
     // get data
-    const url = '';
-    const data = await axios.get(url);
+    const url = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/O-A0001-001?Authorization=CWB-42F0992A-D405-4D6B-AE5C-F9C09A49ABB5&downloadType=WEB&format=JSON';
+    const result = await axios.get(url);
+
+    const data = result.data.cwbopendata;
+
+    // json to html table
+    var html = '<table class="table table-striped">';
+    html += '<tr>';
+    // column name
+    data.location[0].weatherElement.forEach(weather => {
+        html += '<th>' + weather.elementName + '</th>';
+    });
+    html += '</tr>';
+    // row value
+    data.location.forEach(location => {
+        html += '<tr>';
+        location.weatherElement.forEach(weather => {
+            html += '<td>' + weather.elementValue.value + '</td>';
+        });
+        html += '<tr>';
+    });
+    html += '</table>';
+
+    ctx.body = html;
 });
 
 app.listen(3000);
